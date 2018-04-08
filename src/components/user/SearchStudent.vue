f4ed93cf7a05f7c54fca905beeb4e9585c788d47
Robert Dovzan <rdovzan@gmail.com>
2018-04-09 00:10:14 +0200

    Student table added

--- src/components/user/SearchStudent.vue
+++ src/components/user/SearchStudent.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="true">
+    <b-table striped hover :items="items">
       <h3>Search students</h3>
-    </b-form>
+    </b-table>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+
+const items = [
+  { EnrollmentNumber: 6318001, first_name: 'Bob', last_name: 'Bogi', address: "Celovska cesta 30", email: "bb0001@student.uni-lj.si", enrollments: "-"},
+  { EnrollmentNumber: 6318002, first_name: 'Annie', last_name: 'Shaw', address: "Celovska cesta 2", email: "as0001@student.uni-lj.si", enrollments: "-" },
+  { EnrollmentNumber: 6318003, first_name: 'Geneva', last_name: 'Wilson', address: "Celovska cesta 33", email: "gw0001@student.uni-lj.si", enrollments: "-" },
+  { EnrollmentNumber: 6318004, first_name: 'Jami', last_name: 'Carney', address: "Celovska cesta 10", email: "jc0001@student.uni-lj.si", enrollments: "-" }
+]
+
 
 @Component({
   components: {
@@ -36,7 +44,13 @@ import { Component, Vue } from 'vue-property-decorator';
     },
   },
 })
-export default class SearchStudent extends Vue {}
+export default class SearchStudent extends Vue {
+  data () {
+    return {
+      items: items
+    }
+  }
+}
 </script>
 
 <style lang="scss">
