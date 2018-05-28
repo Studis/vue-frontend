bde054933038f1d7e2ec82f9a22c1483db82092e
Domen Gašperlin <domen.gasperlin@gmail.com>
2018-05-28 09:47:45 +0200

    Finnished #25, #26

--- src/components/courses/Course.vue
+++ src/components/courses/Course.vue
@@ -15,7 +15,7 @@
       v-model="content"></results>
 
      <br><br><br><br>
-     <results 
+     <results
       title="Exam applications"
       :indexes="true"
       :content="examEnrollments"
@@ -24,10 +24,59 @@
       :details="details"
       v-model="examEnrollments"
       >
-      <b-dropdown id="ddown1" :text="selectedExamTerm" class="m-md-2" >
-          <b-dropdown-item @click.prevent="updateExamTerms(item)" :key="item" v-for="item in allExamTerms">{{item}}</b-dropdown-item>
-         </b-dropdown>
+      <b-dropdown id="ddow" :text="selectedExamTerm" class="m-md-2" >
+          <b-dropdown-item @click.prevent="updateExamTerms(item)" v-for="item in allExamTerms" :key="item">{{item}}</b-dropdown-item>
+      </b-dropdown>
       </results>
+          <b-form @submit="onSubmit" v-if="true">
+
+<br>
+<div v-if="showTable">
+  <h3>Add exam results</h3>
+   <table class="table" >
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Surname</th>
+      <th scope="col">Name</th>
+      <th scope="col">Enrollment</th>
+      <th scope="col">Score</th>
+      <th scope="col">Mark</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(stud,index) in getContentForEditing" :key="stud.id">
+      
+      <th scope="row">{{index+1}}</th>
+      <td>{{stud.surname}}</td>
+      <td>{{stud.name}}</td>
+      <td>{{stud.enrollment}}</td>
+      <td><b-form-input v-model="getContentForEditing[index].score" type="text" :disabled="isMark"></b-form-input></td>
+      <td><b-form-input v-model="getContentForEditing[index].mark" type="text" :disabled="isScore"></b-form-input></td>
+      <td><b-button class="float-right" type="reset" variant="danger" @click.prevent="returnApply(stud)">Vrni prijavo</b-button></td>
+    </tr>
+  </tbody>
+  
+</table>
+<b-row>
+  <b-col>
+    <b-button type="reset" variant="dark" @click.prevent="goBackToTable">Back</b-button>
+  </b-col>
+  <b-col>
+  <b-button class="float-right" type="reset" variant="warning" @click.prevent="saveResults(false)">Save</b-button>
+  </b-col>
+  </b-row>
+</div>
+ 
+      <div class="clearfix"></div>
+      </b-form>
+      <br>
+      <div v-if="!showTable">
+      <b-btn class="float-left" variant="success" @click.prevent="addScores(data)">Add exam scores</b-btn>
+      <b-btn class="float-right" variant="success" @click.prevent="addMarks(data)">Add exam marks</b-btn>
+      </div>
+
       <br><br><br><br>
       <results 
       title="Scheduled exams"
@@ -59,10 +108,10 @@
         <h3>Dodaj izpitni rok</h3>
         <br>
         <p class="naslov">
-          <b-dropdown id="ddown1" :text="examTerm" class="m-md-2">
-            <b-dropdown-item @click.prevent="changeTerm(1)">1</b-dropdown-item>
-            <b-dropdown-item @click.prevent="changeTerm(2)">2</b-dropdown-item>
-            <b-dropdown-item @click.prevent="changeTerm(3)">3</b-dropdown-item>
+          <b-dropdown id="ddown1" :text="examTerm+''" class="m-md-2">
+            <b-dropdown-item @click.prevent="changeTerm('1')">1</b-dropdown-item>
+            <b-dropdown-item @click.prevent="changeTerm('2')">2</b-dropdown-item>
+            <b-dropdown-item @click.prevent="changeTerm('3')">3</b-dropdown-item>
           </b-dropdown>
           Izpitni rok
         </p>
@@ -112,12 +161,77 @@ export default {
   components: {
     results: Results
   },
+  watch: {
+    getContentForEditing: {
+      handler: function(newVal, oldVal) {
+        console.log(`nev ${JSON.stringify(newVal)} odl ${JSON.stringify(oldVal)}`)
+      },
+      deep: true
+    }
+  },
   computed: {
+    showTable ( ) {
+      return this.isScore && !this.isMark || !this.isScore && this.isMark
+    },
     scheduledAt () {
       return this.examDate + "T" + this.examTime + ":00Z"
+    },
+    getContentForEditing () {
+      return this.examEnrollments.content
     }
   },
   methods: {
+    addScores () {
+      this.isScore = true
+      this.isMark = false
+    },
+    addMarks () {
+      this.isMark = true
+      this.isScore = false
+    },
+    returnApply (el) {
+      let postdata = {
+        cancelEnrollment: true
+      }
+      axios.put(`/exams/enrollments/${el.id}`,postdata)
+      .then(response => {
+        if (response.data) {
+          alert(response.data.message)
+        }
+      }).catch((err) => {
+        alert(err.message)
+      })
+    },
+    goBackToTable() {
+      this.isMark = false
+      this.isScore = false
+      this.load()
+    },
+    saveResults (saveMarks) {
+      let booleanAllSuccess = true
+      for (let el of this.getContentForEditing) {
+        let postdata = {
+        }
+        if (this.isMark) postdata['mark'] = el.mark
+        if (this.isScore) postdata['score'] = el.score
+        axios.put(`/exams/enrollments/${el.id}`,postdata)
+        .then(response => {
+          if (response.data && response.data.message) {
+            alert(`Data for student ${el.name} ${el.surname} with ${el.enrollment} error: ${response.data.message}`)
+            booleanAllSuccess = false
+          }
+          if (booleanAllSuccess && this.getContentForEditing.indexOf(el) == this.getContentForEditing.length - 1) alert("Successfully changed!"); 
+          console.log(response.data)
+        }).catch((err) => {
+          alert(err.message)
+        })
+      }
+
+      
+     },
+    onSubmit () {
+
+    },
     showStudent (s) {
       this.$router.push({ name: 'student', params: {id: s.clickedItem.id} })
     },
@@ -300,6 +414,8 @@ export default {
   },
   data() {
     return {
+      isScore: false,
+      isMark: false,
       allExamTerms: [],
       selectedExamTerm: '',
       originalContent: {},
